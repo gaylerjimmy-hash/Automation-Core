07e9785063204e4a8b9b153b2d3d01e2fc87add9
gaylerjimmy-hash <gayler.jimmy@gmail.com>
2026-07-17 20:13:34 -0500

    Clean up
    Delete duplicates

--- automation-core-milestone1-skeleton-fixed/automation-core-milestone1-skeleton/src/Connection/ConnectionManager.cpp
+++
@@ -1,7 +0,0 @@
-#include "automation_core/Connection/ConnectionManager.h"
-
-namespace automation_core {
-
-// TODO: Implement connection lifecycle, IDs, activity tracking and quarantine.
-
-} // namespace automation_core
